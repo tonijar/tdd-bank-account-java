d1e3f5cc8625dc1037a03fcbc88fe18d5215c531
Toni Jara <toni.jara@gmail.com>
2021-09-15 14:03:48 +0200

    Initial account balance is zero

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,8 +4,15 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 
 public class AccountTest {
+
+    @Test
+    public void initialAccountBalanceIsZero() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+    }
 
     @Test
 	@Disabled
